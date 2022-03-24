583082d6de0296cc31e7c8e3741f097072505c01
layalarosa <layalarosa@gmail.com>
2022-03-23 23:47:26 -0400

    Setting up an MVC Application. Creating the List Page.

--- Models/MockCategoryRepository.cs
+++ Models/MockCategoryRepository.cs
@@ -10,9 +10,9 @@ namespace WebShop.Models
         public IEnumerable<Category> AllCategories =>
             new List<Category>
             {
-                new Category{CategoryId=1, CategoryName="Accion", Description="New Call oF Duty"},
-                new Category{CategoryId=2, CategoryName="Aventura", Description="New God of War"},
-                new Category{CategoryId=3, CategoryName="Terror", Description="Loream"}
+                new Category{CategoryId=1, CategoryName="Accion", Description="Lorem"},
+                new Category{CategoryId=2, CategoryName="Aventura", Description="Lorem"},
+                new Category{CategoryId=3, CategoryName="Terror", Description="Lorem"}
 
             };
     }
